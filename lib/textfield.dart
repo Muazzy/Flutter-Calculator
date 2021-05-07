4cdb5b05d406d987856ffeffbf4db6e16121b336
Muazzam Afaque <you@example.com>
2021-05-07 20:26:19 +0500

    added the whole app

---
+++ lib/textfield.dart
@@ -0,0 +1,40 @@
+// import 'package:flutter/material.dart';
+
+// class DisplayScreen extends StatefulWidget {
+//   @override
+//   _DisplayScreenState createState() => _DisplayScreenState();
+// }
+
+// class _DisplayScreenState extends State<DisplayScreen> {
+//   final textController = TextEditingController();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Expanded(
+//       child: TextField(
+//         controller: textController,
+//         onChanged: (data) {},
+//         // showCursor: true,
+//         readOnly: true,
+//         keyboardType: TextInputType.number,
+//         textAlign: TextAlign.right,
+//         textAlignVertical: TextAlignVertical.bottom,
+
+//         //decoration
+//         decoration: InputDecoration(
+//           border: InputBorder.none,
+//           enabledBorder: InputBorder.none,
+//           errorBorder: InputBorder.none,
+//           focusedBorder: InputBorder.none,
+//           disabledBorder: InputBorder.none,
+//           focusedErrorBorder: InputBorder.none,
+//           contentPadding: EdgeInsets.all(10),
+//           hintText: '0',
+//           hintStyle: TextStyle(
+//             fontSize: 50,
+//             color: Colors.grey[100],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
